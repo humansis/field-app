bd254d8cb00ff8e8a5f0d346441b7b5ee5a84700
Michal Neubauer <michal.neubauer@quanti.cz>
2021-01-25 21:11:11 +0100

    add button for exporting

--- app/src/main/java/cz/applifting/humansis/ui/App.kt
+++ app/src/main/java/cz/applifting/humansis/ui/App.kt
@@ -3,6 +3,12 @@ package cz.applifting.humansis.ui
 import android.app.Application
 import cz.applifting.humansis.di.AppComponent
 import cz.applifting.humansis.di.DaggerAppComponent
+import quanti.com.kotlinlog.Log
+import quanti.com.kotlinlog.android.AndroidLogger
+import quanti.com.kotlinlog.base.LogLevel
+import quanti.com.kotlinlog.base.LoggerBundle
+import quanti.com.kotlinlog.file.FileLogger
+import quanti.com.kotlinlog.file.bundle.DayLogBundle
 
 /**
  * Created by Petr Kubes <petr.kubes@applifting.cz> on 14, August, 2019
@@ -13,6 +19,12 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        // Use custom logger
+        Log.initialise(this)
+        Log.addLogger(AndroidLogger(LoggerBundle(LogLevel.DEBUG)))
+        Log.addLogger(FileLogger(applicationContext, DayLogBundle(maxDaysSaved = 3)))
+        Log.useUncheckedErrorHandler()
 
         appComponent = DaggerAppComponent.builder()
             .context(this)
