3a4d0ebdb9661009a61ce831e32009bf503d71fb
Michal Neubauer <michal.neubauer@quanti.cz>
2021-04-08 10:52:14 +0200

    Improve NFC Initializer behavior

--- app/src/main/java/cz/applifting/humansis/misc/NfcInitializer.kt
+++ app/src/main/java/cz/applifting/humansis/misc/NfcInitializer.kt
@@ -1,48 +1,51 @@
 package cz.applifting.humansis.misc
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.app.PendingIntent
 import android.content.Intent
-import android.content.res.Resources
 import android.nfc.NfcAdapter
 import android.provider.Settings
-import android.widget.Toast
 import cz.applifting.humansis.R
 
-class NfcInitializer(
-        private val activity: Activity
-    ) {
+object NfcInitializer {
 
-    private var pendingIntent: PendingIntent? = null
-
-    fun initNfc(): Boolean {
+    fun initNfc( activity: Activity): Boolean {
         val nfcAdapter = NfcAdapter.getDefaultAdapter(activity)
                 ?: // NFC is not available on this device
                 return false
 
-        pendingIntent = PendingIntent.getActivity(
+        val pendingIntent = PendingIntent.getActivity(
                 activity, 0,
                 Intent(activity, activity.javaClass)
                         .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0
         )
 
-        nfcAdapter.let { nfcAdapter ->
-            if (!nfcAdapter.isEnabled) {
-                showWirelessSettings()
+        nfcAdapter.let {
+            return if (!it.isEnabled) {
+                showWirelessSettings(activity)
+                false
+            } else {
+                it.enableForegroundDispatch(activity, pendingIntent, null, null)
+                true
             }
-            nfcAdapter.enableForegroundDispatch(activity, pendingIntent, null, null)
         }
-
-        return true
     }
 
-    private fun showWirelessSettings() {
-        Toast.makeText(
-                activity,
-                Resources.getSystem().getString(R.string.you_need_to_enable_nfc),
-                Toast.LENGTH_LONG
-        ).show()
-        val intent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
-        activity.startActivity(intent)
+    private fun showWirelessSettings(activity: Activity) {
+
+        AlertDialog.Builder(activity, R.style.DialogTheme)
+                .setMessage(activity.getString(R.string.you_need_to_enable_nfc))
+                .setCancelable(true)
+                .setPositiveButton(activity.getString(R.string.action_settings)) { _,_ ->
+                    activity.startActivity(Intent(Settings.ACTION_NFC_SETTINGS))
+                }
+                .setNegativeButton(activity.getString(R.string.cancel), null)
+                .create()
+                .show();
+    }
+
+    fun disableForegroundDispatch(activity: Activity) {
+        NfcAdapter.getDefaultAdapter(activity)?.disableForegroundDispatch(activity)
     }
 }
