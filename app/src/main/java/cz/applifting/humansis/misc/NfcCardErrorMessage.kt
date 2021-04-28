8f871603a8d82877fcb4305fd753b42334b3892c
Michal Neubauer <michal.neubauer@quanti.cz>
2021-04-28 15:38:34 +0200

    use context instead of activity

--- app/src/main/java/cz/applifting/humansis/misc/NfcCardErrorMessage.kt
+++ app/src/main/java/cz/applifting/humansis/misc/NfcCardErrorMessage.kt
@@ -1,21 +1,21 @@
 package cz.applifting.humansis.misc
 
-import android.app.Activity
+import android.content.Context
 import cz.applifting.humansis.R
 import cz.quanti.android.nfc.exception.PINExceptionEnum
 
 object NfcCardErrorMessage {
-    fun getNfcCardErrorMessage(pinExceptionEnum: PINExceptionEnum, activity: Activity): String {
+    fun getNfcCardErrorMessage(pinExceptionEnum: PINExceptionEnum, context: Context): String {
         return when (pinExceptionEnum) {
-            PINExceptionEnum.CARD_LOCKED -> activity.getString(R.string.card_locked)
-            PINExceptionEnum.INCORRECT_PIN -> activity.getString(R.string.incorrect_pin)
-            PINExceptionEnum.INVALID_DATA -> activity.getString(R.string.invalid_data)
-            PINExceptionEnum.UNSUPPORTED_VERSION -> activity.getString(R.string.invalid_version)
-            PINExceptionEnum.DIFFERENT_CURRENCY -> activity.getString(R.string.currency_mismatch)
-            PINExceptionEnum.TAG_LOST -> activity.getString(R.string.tag_lost_card_error)
-            PINExceptionEnum.DIFFERENT_USER -> activity.getString(R.string.different_user_card_error)
-            PINExceptionEnum.CARD_INITIALIZED -> activity.getString(R.string.card_initialized)
-            else -> activity.getString(R.string.card_error)
+            PINExceptionEnum.CARD_LOCKED -> context.getString(R.string.card_locked)
+            PINExceptionEnum.INCORRECT_PIN -> context.getString(R.string.incorrect_pin)
+            PINExceptionEnum.INVALID_DATA -> context.getString(R.string.invalid_data)
+            PINExceptionEnum.UNSUPPORTED_VERSION -> context.getString(R.string.invalid_version)
+            PINExceptionEnum.DIFFERENT_CURRENCY -> context.getString(R.string.currency_mismatch)
+            PINExceptionEnum.TAG_LOST -> context.getString(R.string.tag_lost_card_error)
+            PINExceptionEnum.DIFFERENT_USER -> context.getString(R.string.different_user_card_error)
+            PINExceptionEnum.CARD_INITIALIZED -> context.getString(R.string.card_initialized)
+            else -> context.getString(R.string.card_error)
         }
     }
 }
