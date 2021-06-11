03b9ff56f6200162f266b449f46ffdc21f595928
neubauerm <20091994aB>
2021-06-11 11:55:45 +0200

    REF: do not call SimpleDateFormat each time

--- app/src/main/java/cz/applifting/humansis/misc/DateUtil.kt
+++ app/src/main/java/cz/applifting/humansis/misc/DateUtil.kt
@@ -4,8 +4,9 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object DateUtil {
+    val format = SimpleDateFormat("dd-MM-yyyy", Locale.US)
+
     fun stringToDate(value: String?): Date? {
-        val format = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
         return value?.let { format.parse(it) }
     }
 }
