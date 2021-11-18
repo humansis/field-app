6a7ff258d62a0a2fcf177cdf6d4a5f91fa873a91
Michal Neubauer <michal.neubauer@quanti.cz>
2021-11-18 20:01:49 +0100

    REF: add comment

--- app/src/main/java/cz/applifting/humansis/model/api/DistributionBeneficiary.kt
+++ app/src/main/java/cz/applifting/humansis/model/api/DistributionBeneficiary.kt
@@ -3,7 +3,7 @@ package cz.applifting.humansis.model.api
 data class DistributionBeneficiary(
     val id: Int,
     val beneficiary: Beneficiary,
-    val distributedAt: String?,
+    val distributedAt: String?, // TODO tyka se jenom smartcarty v teto distribuci, prejmenovat na smartcardDistributedAt
     val currentSmartcardSerialNumber: String?,
     val generalReliefItems: List<Relief>,
     val booklets: List<Booklet>
