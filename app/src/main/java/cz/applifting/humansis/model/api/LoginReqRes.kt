adfbb0d83be4896e9d3d8db892c5411612ff5354
Michal Neubauer <michal.neubauer@quanti.cz>
2021-12-02 20:17:28 +0100

    REF: comment unused parameters

--- app/src/main/java/cz/applifting/humansis/model/api/LoginReqRes.kt
+++ app/src/main/java/cz/applifting/humansis/model/api/LoginReqRes.kt
@@ -9,23 +9,16 @@ data class LoginReqRes(
     var id: Long = 0,
     var username: String = "",
     var password: String = "",
-   // var countries: List<Country> = listOf(), //it is there, but has strange data inside and thus its useless
-    @SerializedName("available_countries")
-    var availableCountries: List<String>? = null,
-
-    @SerializedName("change_password")
-    var email: String = "",
-    var changePassword: Boolean = false,
-    var vendor: String? = null,
-//    var roles: List<Role>? = null,
-    var language: String? = null,
+//    var countries: List<Country> = listOf(), //it is there, but has strange data inside and thus its useless
 //    var projects: List<Project>? = null, //it is there, but has strange data inside and thus its useless
-
-    var transactions: List<Any>? = null,
-    @SerializedName("phone_prefix")
-    var phonePrefix: String? = null,
-    @SerializedName("phone_number")
-    var phoneNumber: String? = null,
-    @SerializedName("two_factor_authentication")
-    var twoFactorAuthentication: Boolean = false
+    var email: String = "",
+//    var roles: List<Role>? = null, // it is there, but not used by the app
+//    var transactions: List<Any>? = null, // it is there, but not used by the app
+//    var vendor: String? = null, // it is there, but not used by the app
+//    var language: String? = null, // it is there, but not used by the app
+//    @SerializedName("phone_prefix") var phonePrefix: String? = null, // it is there, but not used by the app
+//    @SerializedName("phone_number") var phoneNumber: String? = null, // it is there, but not used by the app
+    @SerializedName("change_password") var changePassword: Boolean = false,
+//    @SerializedName("two_factor_authentication") var twoFactorAuthentication: Boolean = false // it is there, but not used by the app
+    @SerializedName("available_countries") var availableCountries: List<String>? = null
 )
