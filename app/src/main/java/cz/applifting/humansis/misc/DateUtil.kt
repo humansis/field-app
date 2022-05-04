c31ed8accf8e2153e48a2e4ff05def2568d78902
Michal Neubauer <michal.neubauer@quanti.cz>
2022-05-04 15:21:58 +0200

    REF: update relief package endpoints

--- app/src/main/java/cz/applifting/humansis/misc/DateUtil.kt
+++ app/src/main/java/cz/applifting/humansis/misc/DateUtil.kt
@@ -7,6 +7,7 @@ import java.util.*
 
 object DateUtil {
     private val format = SimpleDateFormat("dd-MM-yyyy", Locale.US)
+    private val formatForApiRequest = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.US)
 
     fun stringToDate(value: String?): Date? {
         return value?.let { format.parse(it) }
@@ -14,5 +15,9 @@ object DateUtil {
 
     fun dateToString(value: Date, context: Context): String {
         return getDateFormat(context).format(value)
+    }
+
+    fun convertTimeForApiRequestBody(date: Date): String {
+        return formatForApiRequest.format(date)
     }
 }
