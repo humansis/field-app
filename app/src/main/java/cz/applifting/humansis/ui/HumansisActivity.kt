a2881e657acc347b7fe23f82fff7750006628c36
Michal Neubauer <michal.neubauer@quanti.cz>
2022-05-07 17:06:17 +0200

    FEAT: check app version on activity started

--- app/src/main/java/cz/applifting/humansis/ui/HumansisActivity.kt
+++ app/src/main/java/cz/applifting/humansis/ui/HumansisActivity.kt
@@ -16,6 +16,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import androidx.work.*
 import com.google.android.material.navigation.NavigationView
+import cz.applifting.humansis.BuildConfig
 import cz.applifting.humansis.R
 import cz.applifting.humansis.extensions.getDate
 import cz.applifting.humansis.extensions.isWifiConnected
@@ -31,7 +32,6 @@ import cz.applifting.humansis.ui.main.MainViewModel
 import cz.quanti.android.nfc.dto.v2.UserPinBalance
 import cz.quanti.android.nfc.exception.PINException
 import io.reactivex.disposables.Disposable
-import quanti.com.kotlinlog.BuildConfig
 import quanti.com.kotlinlog.Log
 import java.util.*
 import javax.inject.Inject
@@ -75,6 +75,7 @@ class HumansisActivity : BaseActivity(), NfcAdapter.ReaderCallback, NavigationVi
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart")
+        checkAppVersion()
     }
 
     override fun onResume() {
@@ -139,6 +140,21 @@ class HumansisActivity : BaseActivity(), NfcAdapter.ReaderCallback, NavigationVi
             drawer.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
+        }
+    }
+
+    private fun checkAppVersion() {
+        val lastVersion = sp.getString(LAST_VERSION_KEY, null)
+        val currentVersion = BuildConfig.VERSION_NAME
+        when {
+            lastVersion == null -> {
+                Log.d(TAG, "App updated to $currentVersion")
+                sp.edit().putString(LAST_VERSION_KEY, currentVersion).apply()
+            }
+            lastVersion != currentVersion -> {
+                Log.d(TAG, "App updated from $lastVersion to $currentVersion")
+                sp.edit().putString(LAST_VERSION_KEY, currentVersion).apply()
+            }
         }
     }
 
@@ -321,5 +337,6 @@ class HumansisActivity : BaseActivity(), NfcAdapter.ReaderCallback, NavigationVi
 
     companion object {
         private val TAG = HumansisActivity::class.java.simpleName
+        private const val LAST_VERSION_KEY = "last-version-key"
     }
 }
