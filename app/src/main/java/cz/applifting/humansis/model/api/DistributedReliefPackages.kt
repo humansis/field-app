640266254e48aacc051ec958a2a3990af156f3d1
Michal Neubauer <michal.neubauer@quanti.cz>
2022-05-19 14:42:12 +0200

    FIX: return accidentaly removed code

--- app/src/main/java/cz/applifting/humansis/model/api/DistributedReliefPackages.kt
+++ app/src/main/java/cz/applifting/humansis/model/api/DistributedReliefPackages.kt
@@ -2,5 +2,8 @@ package cz.applifting.humansis.model.api
 
 data class DistributedReliefPackages(
     val id: Int,
-    val dateDistributed: String
+    val dateDistributed: String,
+    // Leave amountDistributed null or enter whole amountToDistribute if relief was distributed completely.
+    val amountDistributed: Double? = null // TODO for future usages, such as partial distributions.
+
 )
