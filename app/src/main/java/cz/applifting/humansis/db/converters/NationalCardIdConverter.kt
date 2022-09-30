b98c2b00e98de9871c8237df50dfa10d72a79837
neubauerm <michal.neubauer@quanti.cz>
2022-09-30 14:06:57 +0200

    REF: remove comment

--- app/src/main/java/cz/applifting/humansis/db/converters/NationalCardIdConverter.kt
+++ app/src/main/java/cz/applifting/humansis/db/converters/NationalCardIdConverter.kt
@@ -5,9 +5,6 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import cz.applifting.humansis.model.api.NationalCardId
 
-/**
- * Created by Petr Kubes <petr.kubes@applifting.cz> on 29, September, 2019
- */
 class NationalCardIdConverter {
     @TypeConverter
     fun toList(value: String): List<NationalCardId> {
